5734f3eb9ae65192561c7156ebe4acdd0e2c4b78
luis@yo.es <luis@yo.es>
2015-12-03 13:52:08 +0100

    Con test

---
+++ TestMvc/BasePruebas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubirFicheros.Controllers;
+using SubirFicheros.Models;
+
+namespace TestMvc
+{
+    public abstract class BasePruebas
+    {
+        protected HomeController Controller;
+        protected ficherosEntities db;
+        
+        protected BasePruebas()
+        {
+            db=new ficherosEntities();
+            Controller=new HomeController();
+        }
+
+    }
+}
---
+++ TestMvc/TestbbddFicheros.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SubirFicheros.Models;
+
+namespace TestMvc
+{
+    /// <summary>
+    /// Summary description for TestbbddFicheros
+    /// </summary>
+    [TestClass]
+    public class TestbbddFicheros:BasePruebas
+    {
+        public TestbbddFicheros()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void PruebaConsultaFicherosAzure()
+        {
+            int esperados = 2;
+            var recibidos = db.Ficheros.Count(o => o.tipo == "azure");
+
+            Assert.AreEqual(esperados,recibidos);
+        }
+
+        [TestMethod]
+        public void PruebaConsultaFicherosBinarios()
+        {
+            int n = 0;
+            var f=new Ficheros()
+            {
+                datos = "",
+                datosb = new byte[]{},
+                nombre = "borrar",
+                tipoFichero = 1,
+                tipo = "azure"
+            };
+
+            db.Ficheros.Add(f);
+            try
+            {
+                n=db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Assert.IsFalse(n==0);
+        }
+    }
+}
